4fabf694d4cbd47aa85983f9137a58ab1e596ed6
hopiezzZ <101639185+hopiezzZ@users.noreply.github.com>
2022-03-15 19:24:44 +0800

    Add files via upload

---
+++ myletter.py
@@ -0,0 +1,8 @@
+spaces = ' ' * 25
+print('%s 12Butts Wynd' % spaces)
+print('%s Twinklebottom Heath' % spaces)
+print('%s West Snoring' % spaces)
+print()
+print()
+print('Dear sir')
+print('ku ni ji wa')
